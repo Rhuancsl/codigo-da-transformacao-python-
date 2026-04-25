d432bf4c547449f1d1a4e70c67634b60165a1ff7
Rhuancsl <rhuancs.limacontato@gmail.com>
2026-04-25 19:07:26 -0300

    Add files via upload

---
+++ Modulo_1/codigo_basico.py
@@ -0,0 +1,8 @@
+# Programa básico em Python
+
+def saudacao():
+    nome = input("Digite seu nome: ")
+    print(f"Olá, {nome}! Seja bem-vindo ao seu primeiro projeto no GitHub!")
+
+if __name__ == "__main__":
+    saudacao()
