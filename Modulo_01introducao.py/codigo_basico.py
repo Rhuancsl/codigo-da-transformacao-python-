639b106fdc946d3f7c6fb1d7268293b4837a3258
Rhuancsl <rhuancs.limacontato@gmail.com>
2026-04-25 19:07:01 -0300

    Delete Modulo_01introducao.py directory

--- Modulo_01introducao.py/codigo_basico.py
+++
@@ -1,8 +0,0 @@
-# Programa básico em Python
-
-def saudacao():
-    nome = input("Digite seu nome: ")
-    print(f"Olá, {nome}! Seja bem-vindo ao seu primeiro projeto no GitHub!")
-
-if __name__ == "__main__":
-    saudacao()
